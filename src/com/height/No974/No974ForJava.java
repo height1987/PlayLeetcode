268d5c3b702a3e2b6d93b31d963252ca0ccc2752
zhenyu.yue <hheight@163.com>
2020-10-03 10:27:10 +0800

    解题思路

--- src/com/height/No974/No974ForJava.java
+++ src/com/height/No974/No974ForJava.java
@@ -7,28 +7,32 @@ import java.util.Map;
 /**
  * 974. 和可被 K 整除的子数组
  * https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/
+ * 题目：给定一个整数数组 A，返回其中元素之和可被 K 整除的（连续、非空）子数组的数目。
+ * 解题思路：
+ * 前缀和的模相同的个数，就是能被K整除的子数组的个数
+ * 1.先算出前缀和的摸
+ * 2.通过hashmap获取摸相同的个数
+ *
  */
 
 public class No974ForJava {
-    public static int subarraysDivByK(int[] A, int K) {
-        if(A == null || A.length == 0){
+    public static int subarraysDivByK(int[] array, int num) {
+        if(array == null || array.length == 0){
             return 0;
         }
-        if(K == 0){
+        if(num == 0){
             return 0;
         }
-        int newK = K < 0 ? (K*-1) : K ;
-        BigInteger s = new BigInteger("1");
+        int newK = num < 0 ? (num*-1) : num ;
 
         Map<Integer,Integer> count = new HashMap<Integer,Integer>();
-        char ss  = '1';
 
         count.put(0,1);
         int  preSum = 0;
-        for(int i = 0 ; i< A.length ; i++){
-            int mokNow = ((A[i] + preSum)%newK + newK)%newK;
+        for(int i = 0 ; i< array.length ; i++){
+            int mokNow = ((array[i] + preSum)%newK + newK)%newK;
             count.put(mokNow,count.getOrDefault(mokNow,0) + 1);
-            preSum += A[i];
+            preSum += array[i];
         }
         int allCount = 0;
         for(Map.Entry<Integer,Integer> key : count.entrySet()){
