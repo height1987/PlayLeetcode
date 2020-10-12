2ff8d608013961458a1ad93562df6267594daafd
zhenyu.yue <hheight@163.com>
2020-10-12 23:27:19 +0800

    no 5

---
+++ src/com/height/No5/No5ForJava.java
@@ -0,0 +1,46 @@
+package com.height.No5;
+
+/**
+ * 5. 最长回文子串
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/
+ * 题目：给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 解题思路：通过构造一个记录子串是否为回文串的数组，迭代公式为dp[i][j] = dp[i+1][j-1] && char[i]==char[j]
+ */
+public class No5ForJava {
+    public String longestPalindrome(String s) {
+
+        // 特判
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+}
