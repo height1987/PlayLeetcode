1eb183e93efe6fbaeb4be0486038b4a8ac1ef917
zhenyu.yue <hheight@163.com>
2020-10-13 20:21:18 +0800

    fix

--- src/com/height/No5/No5ForJava.java
+++ src/com/height/No5/No5ForJava.java
@@ -4,43 +4,44 @@ package com.height.No5;
  * 5. 最长回文子串
  * https://leetcode-cn.com/problems/longest-palindromic-substring/
  * 题目：给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
- * 解题思路：通过构造一个记录子串是否为回文串的数组，迭代公式为dp[i][j] = dp[i+1][j-1] && char[i]==char[j]
+ * 解题思路：通过构造一个记录子串是否为回文串的数组，迭代公式为resFlag[i][j] = resFlag[i+1][j-1] && char[i]==char[j]
  */
 public class No5ForJava {
     public String longestPalindrome(String s) {
-
-        // 特判
-        int len = s.length();
-        if (len < 2) {
+        if (s == null) {
+            return "";
+        }
+        if (s.length() < 2) {
             return s;
         }
-        int maxLen = 1;
+
         int begin = 0;
-        // dp[i][j] 表示 s[i, j] 是否是回文串
-        boolean[][] dp = new boolean[len][len];
-        char[] charArray = s.toCharArray();
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
+        int maxLen = 1;
+        boolean[][] resFlag = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            resFlag[i][i] = true;
         }
-        for (int j = 1; j < len; j++) {
+
+        for (int j = 1; j < s.length(); j++) {
             for (int i = 0; i < j; i++) {
-                if (charArray[i] != charArray[j]) {
-                    dp[i][j] = false;
+                if (s.charAt(i) != s.charAt(j)) {
+                    resFlag[i][j] = false;
                 } else {
                     if (j - i < 3) {
-                        dp[i][j] = true;
+                        resFlag[i][j] = true;
                     } else {
-                        dp[i][j] = dp[i + 1][j - 1];
+                        resFlag[i][j] = resFlag[i + 1][j - 1];
                     }
                 }
-                // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
-                if (dp[i][j] && j - i + 1 > maxLen) {
-                    maxLen = j - i + 1;
+                int currentLen = j - i + 1;
+                if (resFlag[i][j] && currentLen > maxLen) {
+                    maxLen = currentLen;
                     begin = i;
                 }
             }
         }
         return s.substring(begin, begin + maxLen);
+
     }
 
 }
