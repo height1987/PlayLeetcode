de8419db038315cad9d18503c76af10235c7a4bb
zhenyu.yue <hheight@163.com>
2020-10-26 23:23:15 +0800

    offer 67

---
+++ src/com/height/jzoffer/No67.java
@@ -0,0 +1,24 @@
+package com.height.jzoffer;
+
+/**
+ * No. 67
+ * 将一个字符串转换成一个整数，字符串不是一个合法的数值则返回 0，要求不能使用字符串转换整数的库函数。
+ * https://github.com/CyC2018/CS-Notes/blob/master/notes/67.%20%E6%8A%8A%E5%AD%97%E7%AC%A6%E4%B8%B2%E8%BD%AC%E6%8D%A2%E6%88%90%E6%95%B4%E6%95%B0.md
+ */
+public class No67 {
+    public int StrToInt(String str) {
+        if (str == null || str.length() == 0)
+            return 0;
+        boolean isNegative = str.charAt(0) == '-';
+        int ret = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (i == 0 && (c == '+' || c == '-'))
+                continue;
+            if (c < '0' || c > '9')
+                return 0;
+            ret = ret * 10 + (c - '0');
+        }
+        return isNegative ? -ret : ret;
+    }
+}
