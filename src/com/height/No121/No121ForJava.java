6b65d207075eaadf18307b317fbda20c213811ea
zhenyu.yue <hheight@163.com>
2020-10-29 15:44:19 +0800

    换成中文链接

--- src/com/height/No121/No121ForJava.java
+++ src/com/height/No121/No121ForJava.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /**
  * 121.  Best Time to Buy and Sell Stock
- * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
  * <p>
  * Say you have an array for which the ith element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
