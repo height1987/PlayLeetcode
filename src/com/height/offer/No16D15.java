a624cd9e4dc8512aa0e75d62bf13cc104555264e
zhenyu.yue <hheight@163.com>
2020-11-12 23:44:44 +0800

    fix

--- src/com/height/offer/No16D15.java
+++ src/com/height/offer/No16D15.java
@@ -1,5 +1,8 @@
 package com.height.offer;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * 面试题 16.25. LRU缓存
  * https://leetcode-cn.com/problems/lru-cache-lcci/
@@ -13,4 +16,35 @@ package com.height.offer;
  */
 
 public class No16D15 {
+
+    int capacity;
+    Map<Integer, Integer> map;
+
+    public No16D15(int capacity) {
+        this.capacity = capacity;
+        map = new LinkedHashMap<>();
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        // 先删除旧的位置，再放入新位置
+        Integer value = map.remove(key);
+        map.put(key, value);
+        return value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            map.remove(key);
+            map.put(key, value);
+            return;
+        }
+        map.put(key, value);
+        // 超出capacity，删除最久没用的,利用迭代器删除第一个
+        if (map.size() > capacity) {
+            map.remove(map.entrySet().iterator().next().getKey());
+        }
+    }
 }
