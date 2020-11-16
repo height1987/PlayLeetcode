bbc853d328804816a9666ac47147dcba810f667c
zhenyu.yue <hheight@163.com>
2020-11-16 23:40:23 +0800

    fix

--- src/com/height/offer/No08D04.java
+++ src/com/height/offer/No08D04.java
@@ -28,4 +28,17 @@ public class No08D04 {
         }
         return res;
     }
+
+    public List<List<Integer>> subsetsB(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int num : nums) {
+            for (int i = 0, j = res.size(); i < j; i++) {
+                List<Integer> one = new ArrayList<>(res.get(i));
+                one.add(num);
+                res.add(one);
+            }
+        }
+        return res;
+    }
 }
