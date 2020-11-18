040dbd358bc2cc12c557bd2c9ca07b04738eef3d
zhenyu.yue <hheight@163.com>
2020-11-18 23:00:40 +0800

    no 08.11

---
+++ src/com/height/No08D11/No08D11ForJava.java
@@ -0,0 +1,23 @@
+package com.height.No08D11;
+
+/**
+ * https://leetcode-cn.com/problems/coin-lcci/
+ * 
+ */
+public class No08D11ForJava {
+    static final int MOD = 1000000007;
+    int[] coins = {25, 10, 5, 1};
+
+    public int waysToChange(int n) {
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int c = 0; c < 4; ++c) {
+            int coin = coins[c];
+            for (int i = coin; i <= n; ++i) {
+                f[i] = (f[i] + f[i - coin]) % MOD;
+            }
+        }
+        return f[n];
+    }
+
+}
